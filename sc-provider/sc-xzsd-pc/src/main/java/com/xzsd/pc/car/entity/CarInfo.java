a6b46b68ce08f460eff5ebe256485785697a02f4
123 <123>
2020-08-27 10:21:26 +0800

    yida1.1

--- sc-provider/sc-xzsd-pc/src/main/java/com/xzsd/pc/car/entity/CarInfo.java
+++ sc-provider/sc-xzsd-pc/src/main/java/com/xzsd/pc/car/entity/CarInfo.java
@@ -1,4 +1,90 @@
 package com.xzsd.pc.car.entity;
 
+import java.util.Date;
+
 public class CarInfo {
+    /**
+     * 车辆编号
+     */
+    private String carId;
+    /**
+     * 车辆名称
+     */
+    private String carName;
+    /**
+     * 创建人
+     */
+    private String createBy;
+    /**
+     * 创建时间
+     */
+    private Date gmtCreate;
+    /**
+     * 修改人
+     */
+    private String lastModifiedBy;
+    /**
+     * 修改时间
+     */
+    private Date gmtModified;
+    /**
+     * 版本号
+     */
+    private String version;
+
+    public String getCarId() {
+        return carId;
+    }
+
+    public void setCarId(String carId) {
+        this.carId = carId;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
 }
